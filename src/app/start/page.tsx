e278cd4fff9626e4f8a5783dc7a0b9776995482b
monkeyK1n9 <mkeyk1n9@gmail.com>
2023-11-13 01:28:46 +0100

    typo fix

--- src/app/start/page.tsx
+++ src/app/start/page.tsx
@@ -1,6 +1,6 @@
 
 export default function StartPage() {
   return (
-    <div style={{backgroundColor: 'blue'}}>StartPage</div>
+    <div style={{backgroundColor: 'blue', color: 'white'}}>StartPage</div>
   )
 }
