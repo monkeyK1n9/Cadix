d5b55fd82858f3d6a6629753fac16f27b4df9eac
monkeyK1n9 <mkeyk1n9@gmail.com>
2024-01-04 09:01:05 +0100

    create not found for profile and change profile page location

---
+++ src/app/profile/[userId]/not-found.tsx
@@ -0,0 +1,16 @@
+import CreditFooter from "@/components/CreditFooter/CreditFooter";
+import WelcomeHeader from "@/components/WelcomeHeader/WelcomeHeader";
+import Link from "next/link";
+
+export default function NotFound() {
+    return (
+        <>
+        <WelcomeHeader />
+        <div className="text-center">
+            <p className="mt-10">Sorry, the requested user does not exist.</p>
+            <Link href="/" className="hover:text-black/70">⬅ Back</Link>
+        </div>
+        <CreditFooter />
+        </>
+    )
+}
